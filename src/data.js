7255cf9d079503460adcb94e69912e78f378350c
Lyudmila <milo4kamoya@gmail.com>
2022-05-11 12:06:50 -0700

    data.js added

---
+++ src/data.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+      title: "Mount Fuji",
+      location: "Japan",
+      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+      startDate: "12 Jan, 2021",
+      endDate: "24 Jan, 2021",
+      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      imageUrl: "mountain.png"
+  },
+  {
+    title: "Sydney Opera House",
+      location: "Australia",
+      googleMapsUrl: "https://goo.gl/maps/HK5T7Yu3M2cCkSRs9",
+      startDate: "27 May, 2021",
+      endDate: "8 Jun, 2021",
+      description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+      imageUrl: "sydney.png"
+  },
+  {
+    title: "Geirangerfjord",
+      location: "Norway",
+      googleMapsUrl: "https://goo.gl/maps/2WLJQuDnfVj4Xvc28",
+      startDate: "01 Oct, 2021",
+      endDate: "18 Nov, 2021",
+      description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+      imageUrl: "fjord.png"
+  },
+  {
+    title: "Prague",
+      location: "Czech Republic",
+      googleMapsUrl: "https://goo.gl/maps/2W3CrVtuz9LiDJQy8",
+      startDate: "07 May, 2018",
+      endDate: "15 May, 2018",
+      description: "Situated in the northwest of the country on the Vltava River, Prague is the capital and the largest city of the Czech Republic. This magical city of bridges, cathedrals, gold-tipped towers and church spires is also the fourteenth largest city in the European Union..",
+      imageUrl: "prague.png"
+  },
+  {
+    title: "Chaweng Beach",
+      location: "Thailand",
+      googleMapsUrl: "https://goo.gl/maps/St5TorgNcs2Nevwu9",
+      startDate: "12 Jul, 2012",
+      endDate: "24 Jul, 2012",
+      description: "Chaweng Beach is the #1 resort on Koh Samui and it has beautiful white sandy beaches stretching for seven kilometers and is the longest on the island.",
+      imageUrl: "chaweng.png"
+  }
+
+]
+
+export default data;
